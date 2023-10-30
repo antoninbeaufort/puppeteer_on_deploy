7cd46e2a041a308e4730519606dbfcd656bf0973
antoninbeaufort <33757018+antoninbeaufort@users.noreply.github.com>
2023-10-30 11:50:55 +0100

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
 export { default as puppeteer } from "https://deno.land/x/puppeteer@9.0.0/mod.ts";
 export { Application, Router } from "https://deno.land/x/oak@v7.3.0/mod.ts";
-export * from "https://esm.sh/preact@10.5.12";
-export { renderToString } from "https://esm.sh/preact@10.5.12";
+export * from "https://esm.sh/preact@10.18.1";
+export { renderToString } from "https://esm.sh/*preact-render-to-string@6.2.2";
